d40b25152d05f06033dd2a228ed2723e069fa187
sJhonny-e <sjhonny@inbox.com>
2018-11-17 13:29:39 +0000

    putting it all together

--- log_parser.rb
+++ log_parser.rb
@@ -1,4 +1,19 @@
 #!/usr/bin/env ruby
 require './app/log_parser'
 
-puts "analyzing log file: #{ARGV[0]}"
+log_file_name = ARGV[0]
+unless log_file_name
+    puts "Usage: ./log_parser.rb <log file name>" 
+    abort
+end
+puts "analyzing log file: #{log_file_name}"
+
+file_content = File.read(log_file_name)
+parser = FileParser.new(file_content)
+
+store = LogLineStore.new
+parser.log_lines.each { |l| store.store_log_line(l) }
+
+totals_visitor = TotalPageVisitsVisitor.new
+uniques_visitor = UniqueViewsVisitor.new
+store.visit_all([totals_visitor, uniques_visitor])
