6216dbf322831851d6756c061f80fdd945c82b7c
sJhonny-e <sjhonny@inbox.com>
2018-11-17 12:51:04 +0000

    basic caching to prevent unecessary repeat parsing

--- app/file_parser.rb
+++ app/file_parser.rb
@@ -4,13 +4,17 @@ class FileParser
     end
 
     def log_lines
+        @log_lines ||= parse_raw_string
+    end
+
+    private
+    attr_reader :raw_string
+
+    def parse_raw_string
         raw_lines = raw_string.split(/\r?\n/)
         raw_lines.map do |raw_line|
             split_line = raw_line.split # by default splits by whitespace
             LogLine.new(split_line[0], split_line[1])
         end
     end
-
-    private
-    attr_reader :raw_string
 end
